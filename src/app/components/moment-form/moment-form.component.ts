7dd710410b097161c8f7e5527fb381618d49ea92
Eduardo Pereira <53060876+Eduardo007-lang@users.noreply.github.com>
2023-10-26 08:24:53 -0300

    fix: added validation formGroup

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,10 +1,44 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-moment-form',
   templateUrl: './moment-form.component.html',
   styleUrls: ['./moment-form.component.css']
 })
-export class MomentFormComponent {
+export class MomentFormComponent implements OnInit {
  @Input() btnText!: string;
+
+ momentForm!: FormGroup;
+
+ constructor(){
+
+ }
+
+
+ ngOnInit(): void {
+     this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl('')
+     });
+ }
+
+ get title(){
+  return this.momentForm.get("title")!;
+ }
+
+ get description(){
+  return this.momentForm.get("description")!;
+ }
+
+ submit(){
+
+  // Verfica se o formulario esta invalido, caso esteja ele retorna a invalid e nao faz o envio.
+    if(this.momentForm.invalid){
+      return;
+    }
+ }
+
 }
